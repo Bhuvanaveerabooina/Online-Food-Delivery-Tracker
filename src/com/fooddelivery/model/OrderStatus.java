907bc2e063e288cf4a3458472b394698c40394d1
Bhuvanaveerabooina <veeraboinabhuvana@gmail.com>
2026-03-09 18:12:53 +0530

    Build console-based Online Food Delivery Tracker app

---
+++ src/com/fooddelivery/model/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.fooddelivery.model;
+
+/**
+ * Represents the lifecycle of an order.
+ */
+public enum OrderStatus {
+    PLACED,
+    PREPARING,
+    OUT_FOR_DELIVERY,
+    DELIVERED
+}
